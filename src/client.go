b04f38a3238399082a0837e8c8ec93ad04aaec32
Ian Obermiller <ian@obermillers.com>
2013-04-18 08:55:03 -0700

    Add client methods for get set and del.

--- src/client.go
+++ src/client.go
@@ -13,7 +13,7 @@ type Client struct {
 func NewClient() *Client {
 	client, err := rpc.DialHTTP("tcp", "localhost"+MasterPort)
 	if err != nil {
-		log.Fatal("dialing: ", err)
+		log.Fatal("Dialing: ", err)
 	}
 	return &Client{client}
 }
@@ -22,10 +22,32 @@ func (c *Client) Get(key string) (value string, err error) {
 	var reply GetResult
 	err = c.rpcClient.Call("Get", &KeyArgs{key}, &reply)
 	if err != nil {
-		log.Fatal("Get error: ", err)
+		log.Fatal("Client Get error: ", err)
 		return
 	}
 	value = reply.Value
 	fmt.Println("Got: ", reply.Value)
+	return
+}
+
+func (c *Client) Set(key string, value string) (err error) {
+	var reply int
+	err = c.rpcClient.Call("Set", &KeyValueArgs{key, value}, &reply)
+	if err != nil {
+		log.Fatal("Client Set error: ", err)
+		return
+	}
+	fmt.Println("Set: ", key, " = ", value)
+	return
+}
+
+func (c *Client) Del(key string) (err error) {
+	var reply int
+	err = c.rpcClient.Call("Del", &KeyArgs{key}, &reply)
+	if err != nil {
+		log.Fatal("Client Del error: ", err)
+		return
+	}
+	fmt.Println("Deleted: ", key)
 	return
 }
