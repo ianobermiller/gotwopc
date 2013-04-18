847a7457a85298578efcb87f87c571bcc7c93404
Ian Obermiller <ian@obermillers.com>
2013-04-18 02:01:47 -0700

    Try out leveldb, only to find that it doesn't work on win64...

--- src/keyValueStore.go
+++ src/keyValueStore.go
@@ -1,85 +1,42 @@
 package gotwopc
 
 import (
-	"database/sql"
+	"code.google.com/p/leveldb-go/leveldb"
+	leveldbdb "code.google.com/p/leveldb-go/leveldb/db"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type keyValueStore struct {
-	db      *sql.DB
-	putStmt *sql.Stmt
-	delStmt *sql.Stmt
-	getStmt *sql.Stmt
+	db *leveldb.DB
 }
 
 func newKeyValueStore(dbPath string) (store *keyValueStore, err error) {
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := leveldb.Open("./testdb", &leveldbdb.Options{})
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Couldn't create db: %q\n", err)
 		return
 	}
 
-	_, err = db.Exec("create table if not exists data (key text not null primary key, value text)")
-	if err != nil {
-		fmt.Printf("Couldn't create table: %q\n", err)
-		return
-	}
-
-	putStmt, err := db.Prepare("insert or replace into data(key, value) values(?, ?)")
-	if err != nil {
-		fmt.Printf("Couldn't prepare put statement: %q\n", err)
-		return
-	}
-
-	delStmt, err := db.Prepare("delete from data where key = ?")
-	if err != nil {
-		fmt.Printf("Couldn't prepare put statement: %q\n", err)
-		return
-	}
-
-	getStmt, err := db.Prepare("select value from data where key = ?")
-	if err != nil {
-		fmt.Printf("Couldn't prepare put statement: %q\n", err)
-		return
-	}
-
-	store = &keyValueStore{db, putStmt, delStmt, getStmt}
+	store = &keyValueStore{db}
 	return
 }
 
 func (s *keyValueStore) put(key string, value string) (err error) {
-	_, err = s.putStmt.Exec(key, value)
+	err = s.db.Set([]byte(key), []byte(value), nil)
 	return
 }
 
 func (s *keyValueStore) del(key string) (err error) {
-	_, err = s.delStmt.Exec(key)
+	err = s.db.Delete([]byte(key), nil)
 	return
 }
 
 func (s *keyValueStore) get(key string) (value string, err error) {
-	row := s.getStmt.QueryRow(key)
-	err = row.Scan(&value)
+	bytes, err := s.db.Get([]byte(key), nil)
+	value = string(bytes)
 	return
 }
 
 func (s *keyValueStore) close() {
 	s.db.Close()
-}
-
-func queryPrepared(db *sql.DB, sql string, args ...interface{}) (result sql.Result, err error) {
-	stmt, err := db.Prepare(sql)
-	if err != nil {
-		fmt.Printf("Couldn't prepare statement: %q\n", err)
-		return
-	}
-	defer stmt.Close()
-
-	result, err = stmt.Exec(args...)
-	if err != nil {
-		fmt.Printf("Couldn't execute statement: %q\n", err)
-		return
-	}
-	return
 }
