1e45b834dc024c5d68c940490496a6d6e58f0197
Ian Obermiller <ian@obermillers.com>
2013-05-01 18:08:16 -0700

    Remove diskv dependency, do file io directly.

--- src/keyValueStore.go
+++ src/keyValueStore.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-	"github.com/peterbourgon/diskv"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
 )
 
 type keyValueStore struct {
-	d *diskv.Diskv
+	basePath string
 }
 
-func flatTransform(s string) []string { return []string{} }
-
 func newKeyValueStore(dbPath string) (store *keyValueStore) {
-	d := diskv.New(diskv.Options{
-		BasePath:     dbPath,
-		Transform:    flatTransform,
-		CacheSizeMax: 1024 * 1024,
-	})
-
-	store = &keyValueStore{d}
+	err := os.MkdirAll(dbPath, 0)
+	if err != nil {
+		log.Fatalln("newKeyValueStore:", err)
+	}
+	store = &keyValueStore{dbPath}
 	return
 }
 
+func (s *keyValueStore) getPath(key string) string {
+	return path.Join(s.basePath, key)
+}
+
 func (s *keyValueStore) put(key string, value string) (err error) {
-	err = s.d.Write(key, []byte(value))
+	err = ioutil.WriteFile(s.getPath(key), []byte(value), 0777)
 	return
 }
 
 func (s *keyValueStore) del(key string) (err error) {
-	err = s.d.Erase(key)
+	err = os.Remove(s.getPath(key))
 	return
 }
 
 func (s *keyValueStore) get(key string) (value string, err error) {
-	bytes, err := s.d.Read(key)
+	bytes, err := ioutil.ReadFile(s.getPath(key))
+	if err != nil {
+		return
+	}
 	value = string(bytes)
 	return
 }
